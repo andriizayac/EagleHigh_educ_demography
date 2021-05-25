a38a599b910b271464c78fd262b987a69c3c0434
[Andrii Zaiats] <[andriizaiats@u.boisestate.edu]>
2021-05-25 09:13:05 -0600

    transfered ui and server codes for the app

---
+++ server.R
@@ -0,0 +1,99 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(deSolve)
+library(rgdal)
+library(raster)
+library(viridis)
+library(png)
+library(rphylopic)
+
+# === load aspect raster
+asp <- raster(paste0(getwd(),"/www/dcewaspect.tif"))
+northness <- (cos(asp*pi/180)+1)/2
+idx <- is.na(northness) == FALSE 
+mat <- northness
+# === load RGB image
+dcew <- stack(paste0(getwd(),"/www/dcewRGB.tif"))
+dcew[values(dcew[[1]]) == 0] <- NA
+
+# define an ODE function and covariates
+year0 <- 1996
+model <- function (time, u0, parms) {
+  with(as.list(c(u0, parms)), {
+    gr = -r/1.1; ch = -k/2
+    h = r + eta*gr
+    g = k + gamma*ch
+    
+    dN <-   h * N * (1 - N / g) 
+    list(dN) })
+}
+u0 <- c(N = 1)
+# times = seq(0, 10, 1)
+# parms <- c(r = .1, k = 10, gamma = 0.5, eta = 0.7)
+# out <- ode(u0, times, model, parms)
+# plot(out, xaxt = "n", xlim = c(-5,max(times)))
+# axis(1, at=c((-5:max(times)) + 10) )
+# lines(out, col = "purple")
+
+# === load icons
+#grass <- readPNG(paste0(getwd(),"/www/grass.png"))
+grass <- image_data("4a00d067-54e4-45dd-abce-9a0b606a34dd", size = 512)[[1]]
+cow <- image_data("dc5c561e-e030-444d-ba22-3d427b60e58a", size = 512)[[1]]
+
+shinyServer(function(input, output) {
+  ldat <- reactive({
+    year = c(year0:input$T)  
+    u0 = c(N = 1)
+    times = seq(0, input$T, 1)
+    params = c(r = input$r, k = input$k, 
+               gamma = input$cheat, eta = input$graze) 
+    return(ode(u0, times, model, params))
+    
+  })
+  output$growthPlot <- renderPlot({
+    out  <- ldat()
+    par(xpd=TRUE, mar = c(5, 5, 9, 1))
+    # plot the solution
+    plot(out, xlim = c(-10, input$T), ylim = c(0, input$k + 1), xaxt = "n",
+         lwd = 5, main = "", xlab = "Years", ylab = "Sagebrush cover, %", cex.lab = 1.5)
+    axis(1, at = c(-10:input$T), labels = c(-10:input$T + 1996))
+    lines(x = c(-10, input$T), y = rep(out[dim(out)[1],2], length(c(-10, input$T))), lty = "dashed", col = "gray")
+    lines(c(-10, -1, 0), c(rep(input$k, 2), 1), lwd=5, lty = "dashed", col = rgb(1,0,1, .5))
+    
+    for(i in 1:round(input$graze*10)) {
+      add_phylopic_base(cow, x = rnorm(1, .7, .1), y = rnorm(1, 1.3, 0.1), 
+                        ysize = rnorm(1, .05, .02) + input$graze*.2, alpha = 1, color = rgb(139/255,69/255,19/255, 1))
+    }
+    for(i in 1:round(input$cheat*20)) {
+      add_phylopic_base(grass, x = rnorm(1, .17, .1), y = rnorm(1, 1.3, .01),
+                        ysize = rnorm(1, .03, 0.02) + input$cheat*.075, alpha = 1, color = "darkgreen")
+    }
+    
+  }, width = 620, height = 400)
+  output$landscapePlot <- renderPlot({
+    out  <- ldat()
+    mat[idx] = out[input$T,2] - northness[idx]*(1-input$aspect)*input$k
+    
+    mat[mat < 0] <- u0
+    cuts <- round(seq(0, input$k, l = input$k))
+    par(mfrow = c(1, 2))
+    mtext("My 'Title' in a strange place", side = 3, line = -6, outer = TRUE)
+    raster::plotRGB(dcew, margins = T, main = "Satellite View")
+    raster::plot(mat, breaks = cuts, col = rev(viridis(input$k)), 
+                 box = F, axes = F, main = "Sagebrush cover, %")
+    par()
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,70 @@
+# This is an application to demonstrate demographic growth of big sagebrush in Boise foothills
+
+# for references: http://shiny.rstudio.com
+# path <- "~/Desktop/CodingClub/ShinyApps/MurdockEduc_demography/MurdockEduc_demography/"
+library(shiny)
+# This is an application to demonstrate demographic growth of big sagebrush in Boise foothills
+
+shinyUI(fluidPage(
+  # Application title
+  titlePanel("Big Sagebrush and Wildfires", windowTitle = "Sage Growth"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("T",
+                  "Years:", min = 10, max = 100, 
+                  value = 10),
+      sliderInput("r",
+                  "Growth rate (r):", min = 0.01, max = 2,
+                  value = .2),
+      sliderInput("k", 
+                  "Carrying capacity (K):", min = 3, max = 50, 
+                  value = 20),
+      sliderInput("aspect",
+                  "Snow accumulation", min = 0, max = 1, 
+                  value = 0),
+      sliderInput("cheat",
+                  "Cheatgrass:", min = 0, max = 1,
+                  value = 0),
+      sliderInput("graze",
+                  "Grazing:", min = 0, max = 1, 
+                  value = 0)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      tabsetPanel(type = "tabs", 
+                  tabPanel("Intro", 
+                           h5("Late August in 1996 was out of ordinary for Boise residents. Four days, August 26-30, a wildfire started during a target shooting raged through Boise foothills around the Military Reserve Park and burning more than 15,000 acres. After the fire had scorched the vegetation on the slopes, the city residents feared of severe mudslides to follow. The fears were ground in what Boise experienced just a few decades before, in 1959, when mud from the hills slid down and flooded residential homes."),
+                           # br(), 
+                           img(src='drawing1.png', align = "center"),
+                           br(), br(),
+                           h5("Boise foothills, Summer 2020"),
+                           img(src='drawing2.png')),
+                  tabPanel("Sagebrush", h5("Because of the mudlisde threat, the city of Boise planned an immediate response to stabilize the soils and restore the vegetation in the foothills. In this lab, we will explore the recovery of big sagebrush, a native shrub in the foothills and the Great Basin, that stabilizes soils and provides essential habitat and forage for wildlife. 
+                                           
+                                           Explore how fast big sagebrush can recover using an equation for logistic population growth, and interactive sliders on the left. For example, drag the «Year» slider to the right to see what big sagebrush cover may look like in 2050! Feel free to also change «r» and «K» parameters (we will explore the rest of sliders in a minute). How do r and K change the curve of recovery?"),
+                           # withMathJax("$$ \\text{Logistic Growth Equation:} \\quad \\frac{dN}{dt} = rN(1 - \\frac{N}{K})$$"),
+                           
+                           plotOutput("growthPlot"),
+                           h5("Let’s see how the city of Boise can plan rehabilitation measures to speed up the recovery of big sagebrush. There are several things to consider. First, invasive species, Cheatgrass, has spread widely throughout the Great Basin and the Boise foothills that can drastically change how big sagebrush recovers. Secondly, grazing can also slow down the process. Explore the effects of  «Cheatgrass» and «Grazing» parameters. Do they change the recovery curve in the same way? 
+                              
+                              On the other hand, management intervention and ecological restoration can greatly facilitate the recovery. Compare the predicted abundance of big sagebrush abundance for 2030 under two scenarios: (i) without Cheatgrass (slider set to 0), and (ii) with moderate Cheatgrass invasion (slider set to 0.5). Keep the sliders for growth rate and carrying capacity parameters set to 0.1 and 25, respectively. 
+                              
+                              Write a letter to the **city with a recommendation for how they can respond to cheatgrass invasion. In your proposed restoration project provide options restoration crews can focus on to compensate for Cheatgrass invasion. You will need to explain and defend your findings for future sagebrush recovery.")), 
+                  tabPanel("Map", h4("Boise foothills in early Spring"),
+                           img(src='drawing3.png'),
+                           br(), h5("Consider the picture above and the two maps. On the left is the view of 
+                                    Boise foothills from a satellite. On the right, same area only now
+                                    showing the abundance of big sagebrush. Experiment with the Snow slider 
+                                    and see how snow accumulation affects sagebrush growth. What 
+                                    would happen if the snow was envenly distributed on the ground and why 
+                                    this is not the case?"),
+                           plotOutput("landscapePlot")
+                           ) 
+                  )
+      
+                  )
+                  )
+))
